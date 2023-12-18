9bc8dcf74983020cbac6c124c00edd129cbf7868
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2023-12-18 10:32:33 +0100

    Update setup_AERMC.py

--- setup_AERMC.py
+++ setup_AERMC.py
@@ -8,25 +8,6 @@ from tqdm.notebook import tqdm
 
 fastq_dir = Path('/store/sdsc/sd29/med_data_wp3/AERMC')
 
-# Modify the pattern to match gzipped files with ".fastq.gz" extension
-forward_reads_files = list(fastq_dir.glob("*R1.fastq.gz"))
-reverse_reads_files = list(fastq_dir.glob("*R2.fastq.gz"))
-
-forward_reads_files.sort()
-reverse_reads_files.sort()
-
-assert len(forward_reads_files) == len(reverse_reads_files)
-
-def get_sample_name(filename: str) -> str:
-    regex = r'\d+_NB\d+_A_L1-4_AIMI-\d+_(R[12])\.fastq\.gz'
-    matches = re.findall(pattern=regex, string=filename)
-    if len(matches) != 1:
-        print(f"Filename '{filename}' does not match the pattern.")
-        return ''  # Or any other handling you need
-
-    sample_name = matches[0].split("_R")[0]
-    return sample_name
-
 # Your filenames, each on a new line
 all_filenames = [
     "210920_SN1126_A_L001_AIMI-393_R1.fastq.gz",
@@ -68,6 +49,24 @@ all_filenames = [
     "220201_NB501473_A_L1-4_AIMI-468_R1.fastq.gz",
     "220201_NB501473_A_L1-4_AIMI-468_R2.fastq.gz"
 ]
+
+forward_reads_files = [fastq_dir / filename for filename in all_filenames if "_R1.fastq.gz" in filename]
+reverse_reads_files = [fastq_dir / filename for filename in all_filenames if "_R2.fastq.gz" in filename]
+
+forward_reads_files.sort()
+reverse_reads_files.sort()
+
+assert len(forward_reads_files) == len(reverse_reads_files)
+
+def get_sample_name(filename: str) -> str:
+    regex = r'\d+_NB\d+_A_L1-4_AIMI-\d+_(R[12])\.fastq\.gz'
+    matches = re.findall(pattern=regex, string=filename)
+    if len(matches) != 1:
+        print(f"Filename '{filename}' does not match the pattern.")
+        return ''  # Or any other handling you need
+
+    sample_name = matches[0].split("_R")[0]
+    return sample_name
 
 for filename in all_filenames:
     sample_name = get_sample_name(filename)
