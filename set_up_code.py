c9f01a769ffc72da095cda1d68eb7639f6a741ec
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2023-12-18 17:42:51 +0100

    Update set_up_code.py

--- set_up_code.py
+++ set_up_code.py
@@ -28,7 +28,6 @@ with open(filename, 'r') as file:
 # Print the list of filenames
 print(all_filenames)
 
-
 forward_reads_files = [fastq_dir / filename for filename in all_filenames if "_R1.fastq.gz" in filename]
 reverse_reads_files = [fastq_dir / filename for filename in all_filenames if "_R2.fastq.gz" in filename]
 
@@ -42,14 +41,12 @@ num_files = len(forward_reads_files)
 print("Forward files:", forward_reads_files)
 print("Reverse files:", reverse_reads_files)
 
-# Inside the loop for processing each file
+# Inside the loop
 for i in tqdm(range(num_files)):
     file = forward_reads_files[i]
     print("Processing:", file)
-    
-for i in tqdm(range(num_files)):
-    file = forward_reads_files[i]
-    sample_name = get_sample_name(file.name)
+    # Extract sample name from the filename directly
+    sample_name = file.stem.split('_R')[0]
     ids = []
     seqs = []
 
@@ -65,7 +62,18 @@ for i in tqdm(range(num_files)):
 
     df['Reverse'] = ''
     file = reverse_reads_files[i]
-    assert sample_name == get_sample_name(file.name)
+    # Extract sample name from the filename directly
+    assert sample_name == file.stem.split('_R')[0]
+
+    with gzip.open(file, 'rt') as handle:
+        for record in SeqIO.parse(handle, format='fastq'):
+            id = record.id
+            seq = record.seq.lower()
+
+            if id in df.index:
+                df.at[id, 'Reverse'] = str(seq)
+            else:
+                warnings.warn("ID of reverse read could not be matched to any forward read.")
 
     with gzip.open(file, 'rt') as handle:
         for record in SeqIO.parse(handle, format='fastq'):
