2f70d059b7d2e1543f8f6dff2ab68a4b084565d1
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-01-16 09:59:13 +0100

    Update wtf_4.py

--- wtf_4.py
+++ wtf_4.py
@@ -102,7 +102,7 @@ def process_file(directory, filename, reference_df, samples_to_exclude, unique_s
 
 def save_to_csv(unique_sample_dataframes, directory):
     logging.info("Saving CSVs")
-    for unique_sample_name, data in tqdm(unique_sample_dataframes.items(), desc="Saving CSVs"):
+    for unique_sample_name, data in tqdm(list(unique_sample_dataframes.items()), desc="Saving CSVs"):
         df = pd.DataFrame(data={'Forward': data['seqs_forward'], 'Reverse': data['seqs_reverse']}, index=data['ids'])
         store_dir = Path(directory) / 'MED_SAMPLES_CSV' / unique_sample_name
         store_dir.mkdir(parents=True, exist_ok=True)
