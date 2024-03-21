8f8bb890da30d4cf62028254d5a80d439c32e1e6
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-03-21 14:52:34 +0100

    Create batch_repasse_script.sh

---
+++ batch_repasse_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+# Here you can put your options (particularly job-name, time, output, and error)
+# DONT REMOVE THE # IN FRONT OF THE SBATCH
+
+#SBATCH --job-name=letizias-job
+#SBATCH --time=12:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --constraint=gpu
+#SBATCH --output=letizias-logfile-%j.log
+#SBATCH --error=letizias-errorfile-%j.err
+#SBATCH --account=sd29
+
+# Here you can put your modules. You can also load PyTorch but in that case it can't be installed in your conda environment
+module load daint-gpu
+module load cray-python
+# module load PyTorch 
+
+# Here you can just put the srun command you would execute. Just replace test_main.py with your script you want to run
+# Make sure to replace <your conda environment> below with the name of the conda environment you want to use
+srun -ul $HOME/miniconda3/envs/zioboia/bin/python wtf_20.py "$1" "$2" -u
